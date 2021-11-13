e14f615b7dd0bbcbe080ab89f474c4501016b740
Chau Cottrell <chauchac98@gmail.com>
2021-11-13 03:02:18 -0800

    Mongoose Part 3

--- Desktop/NucampFolder /5-NodeJS-Express-MongoDB/node-mongoose/index.js
+++ Desktop/NucampFolder /5-NodeJS-Express-MongoDB/node-mongoose/index.js
@@ -5,6 +5,7 @@ const url = 'mongodb://localhost:27017/nucampsite';
 
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -18,10 +19,26 @@ connect.then(() => {
     })
     .then(campsite => {
         console.log(campsite);
-        return Campsite.find();
+
+        return Campsite.findByIdAndUpdate(campsite._id, {
+            $set: { description: 'Updated Test Document' }
+        }, {
+            new: true
+        });
     })
-    .then(campsites => {
-        console.log(campsites);
+    .then(campsite => {
+        console.log(campsite);
+
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificent view!',
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save();
+    })
+    .then(campsite => {
+        console.log(campsite);
         return Campsite.deleteMany();
     })
     .then(() => {
