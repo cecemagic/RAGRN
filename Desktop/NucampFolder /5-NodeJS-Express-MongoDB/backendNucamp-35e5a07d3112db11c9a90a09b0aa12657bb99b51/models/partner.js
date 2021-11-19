b68e0e05a3975349f44898456ea66acf1af5c989
Chau Cottrell <chauchac98@gmail.com>
2021-11-18 16:06:58 -0800

    Basic authentication

---
+++ Desktop/NucampFolder /5-NodeJS-Express-MongoDB/backendNucamp-35e5a07d3112db11c9a90a09b0aa12657bb99b51/models/partner.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const partnerSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+//method create Model and bind it to Schema
+const Partner = mongoose.model('Partner', partnerSchema)
+
+module.exports = Partner;
