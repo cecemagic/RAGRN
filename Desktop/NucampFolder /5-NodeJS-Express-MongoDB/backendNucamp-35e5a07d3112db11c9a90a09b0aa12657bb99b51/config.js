4d33bf7f13291db8554c2c39c13f5c87d1510cba
Chau Cottrell <chauchac98@gmail.com>
2021-11-28 15:07:55 -0800

    Added back config.js

---
+++ Desktop/NucampFolder /5-NodeJS-Express-MongoDB/backendNucamp-35e5a07d3112db11c9a90a09b0aa12657bb99b51/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '2992028617677281',
+        clientSecret: 'cc80dbd36681d0dc9e70271da5e0d'
+    }
+}
